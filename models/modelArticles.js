c87941861010b42d0d97808c55154d436e4d0f74
FredR0101 <fredreed77@gmail.com>
2024-02-20 15:21:08 +0000

    completed section 5

--- models/modelArticles.js
+++ models/modelArticles.js
@@ -15,4 +15,37 @@ function retrieveArticleDataById(article_id) {
     });
 }
 
-module.exports = { retrieveArticleDataById };
+function retrieveAllArticleData(sort_by = "created_at", order = "DESC") {
+  const validOrders = ["ASC", "DESC"];
+
+  if (!validOrders.includes(order)) {
+    return Promise.reject({ status: 400, msg: "bad request" });
+  }
+
+  const validSortBy = ["author", "topic", "created_at"];
+
+  if (!validSortBy.includes(sort_by)) {
+    return Promise.reject({ status: 400, msg: "bad request" });
+  }
+
+  let sqlString = `SELECT 
+        articles.author,
+        articles.title,
+        articles.article_id,
+        articles.topic,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+        COUNT(comments.comment_id) AS comment_count
+        FROM articles
+        LEFT JOIN comments ON articles.article_id = comments.article_id
+        GROUP BY articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url`;
+
+  sqlString += ` ORDER BY ${sort_by} ${order}`;
+
+  return db.query(sqlString).then((result) => {
+    return result.rows;
+  });
+}
+
+module.exports = { retrieveArticleDataById, retrieveAllArticleData };
